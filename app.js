dc3f7830c738b5dffcf16457d33fdc3214e249f6
oscar-salmeron <royalprestigemiracle@gmail.com>
2026-06-19 14:04:44 -0700

    MENUE

--- app.js
+++ app.js
@@ -14,13 +14,74 @@
     '/distribuidor': 'spa/distribuidor.html',
     '/unete': 'spa/unete.html',
     '/contacto': 'spa/contacto.html',
+    '/aviso-legal': 'spa/aviso-legal.html',
+
+    /* LÍNEAS */
+    '/linea-novel': 'spa/linea-novel.html',
+    '/linea-innove': 'spa/linea-innove.html',
+    '/linea-5capas': 'spa/linea-5capas.html',
+    '/linea_easy_release': 'spa/linea_easy_release.html',
+    '/elite-cooking-system': 'spa/elite-cooking-system.html',
+    '/electrodomesticos': 'spa/electrodomesticos.html',
+    '/cuchillos-serie-precision-3': 'spa/cuchillos-serie-precision-3.html',
+    '/sistemas-filtracion': 'spa/sistemas-filtracion.html',
+    '/accesorios': 'spa/accesorios.html',
+
+    /* PRODUCTOS ESPECÍFICOS */
+    '/producto-novel-5': 'spa/producto-novel-5.html',
+    '/producto-novel-7': 'spa/producto-novel-7.html',
+    '/producto-novel-8': 'spa/producto-novel-8.html',
+    '/producto-novel-10': 'spa/producto-novel-10.html',
+    '/producto-novel-15': 'spa/producto-novel-15.html',
+    '/producto-novel-ollas-grandes': 'spa/producto-novel-ollas-grandes.html',
+    '/producto-novel-gourmet': 'spa/producto-novel-gourmet.html',
+    '/piezas-individuales-novel': 'spa/piezas-individuales-novel.html',
+
+    '/innove-juego-5': 'spa/innove-juego-5.html',
+    '/innove-juego-7': 'spa/innove-juego-7.html',
+    '/innove-juego-8': 'spa/innove-juego-8.html',
+    '/innove-juego-10': 'spa/innove-juego-10.html',
+    '/innove-ollas-grandes': 'spa/innove-ollas-grandes.html',
+    '/innove-gourmet': 'spa/innove-gourmet.html',
+    '/innove-piezas': 'spa/innove-piezas.html',
+
+    '/5capas-5-esencial': 'spa/5capas-5-esencial.html',
+    '/5capas-5-complementario': 'spa/5capas-5-complementario.html',
+    '/5capas-7': 'spa/5capas-7.html',
+    '/5capas-8': 'spa/5capas-8.html',
+    '/5capas-10': 'spa/5capas-10.html',
+    '/5capas-ollas-grandes': 'spa/5capas-ollas-grandes.html',
+    '/5capas-gourmet': 'spa/5capas-gourmet.html',
+    '/5capas-piezas-individuales': 'spa/5capas-piezas-individuales.html',
+
+    '/easy_release_sartenes_6p': 'spa/easy_release_sartenes_6p.html',
+    '/easy_release_ollas_6p': 'spa/easy_release_ollas_6p.html',
+
     '/elite-5piezas': 'spa/elite-5piezas.html',
     '/elite-sarten-8': 'spa/elite-sarten-8.html',
     '/elite-base-magnetica': 'spa/elite-base-magnetica.html',
     '/elite-protectores': 'spa/elite-protectores.html',
     '/elite-aros-silicromaticos': 'spa/elite-aros-silicromaticos.html',
     '/elite-valvula-reditemp': 'spa/elite-valvula-reditemp.html',
-'/elite-cooking-system': 'spa/elite-cooking-system.html'
+
+    '/power-blender-max': 'spa/power-blender-max.html',
+    '/power-blender-go': 'spa/power-blender-go.html',
+    '/extractor-de-jugo': 'spa/extractor-de-jugo.html',
+    '/cocina-de-induccion': 'spa/cocina-de-induccion.html',
+
+    '/cuchillos-serie-precision-3-juegos': 'spa/cuchillos-serie-precision-3-juegos.html',
+    '/cuchillos-serie-precision-3-piezas-accesorios': 'spa/cuchillos-serie-precision-3-piezas-accesorios.html',
+
+    '/frescapure-5500': 'spa/frescapure-5500.html',
+    '/frescapure-3500': 'spa/frescapure-3500.html',
+    '/frescaflow': 'spa/frescaflow.html',
+    '/frescapure-shower': 'spa/frescapure-shower.html',
+    '/filtracion-aire': 'spa/filtracion-aire.html',
+
+    '/juegos_servir': 'spa/juegos_servir.html',
+    '/cafe_te_chocolate': 'spa/cafe_te_chocolate.html',
+    '/miscelaneo': 'spa/miscelaneo.html',
+    '/almacenamiento': 'spa/almacenamiento.html'
   };
 
  const realPages = {};
